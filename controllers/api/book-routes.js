7e595ce286435698e9832088b601d6eb73466ac4
Kyle Reynolds <kbreyn06@gmail.com>
2022-06-21 23:32:26 -0500

    book routes added

---
+++ controllers/api/book-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Book } = require('../../models');
+
+
+router.get('/', (req, res) => {
+    Book.findAll()
+      .then(dbBookData => res.json(dbBookData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+
+
+
+
+
+module.exports = router;
