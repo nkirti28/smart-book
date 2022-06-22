6e972358645dcd9ffb260a51495c60f32940ecf0
nkirti28 <nalawade.kirti@gmail.com>
2022-06-22 13:59:16 -0500

    associations added to usermodel

--- models/index.js
+++ models/index.js
@@ -1,31 +1,31 @@
 // import models
-const Book = require('./Book');
-const Category = require('./Category');
-const User = require('./User');
-const UserCart = require('./ShoppingCart');
+const Book = require("./Book");
+const Category = require("./Category");
+const User = require("./User");
+const ShoppingCart = require("./ShoppingCart");
 
 // Book belongsTo Category
 Book.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: "category_id",
 });
 
 // Categories have many Books
 Category.hasMany(Book, {
-  foreignKey: 'category_id'
+  foreignKey: "category_id",
 });
 
 // Books belongToMany Users (through ShoppingCart)
 
-Book.belongsToMany(User, {
-  through: UserCart,
-  foreignKey: 'book_id'
+Book.belongsTo(User, {
+  through: ShoppingCart,
+  foreignKey: "user_id",
 });
 
 // Users belongToMany Books (through ShoppingCart)
 
 User.belongsToMany(Book, {
   through: ShoppingCart,
-  foreignKey: 'user_id'
+  foreignKey: "book_id",
 });
 
 module.exports = {
