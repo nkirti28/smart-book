3b523815af04d740939e70d86184faf0b2934e8b
MattGrizzly <magrishams@gmail.com>
2022-06-22 16:04:50 -0500

    added await

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,21 +1,20 @@
-function signupFormHandler(event) {
-    event.preventDefault();
+await function signupFormHandler(event) {
+  event.preventDefault();
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
+  if (username && email && password) {
+    await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
         headers: { 'Content-Type': 'application/json' }
-      }).then((response) => {console.log(response)})
-    }
+    }).then((response) => {console.log(response)})
   }
+}
 
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
