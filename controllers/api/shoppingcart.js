e7022e36913657c59cc20e9830d86dc00c1b1690
nkirti28 <nalawade.kirti@gmail.com>
2022-06-22 15:55:36 -0500

    added shopping cart routes

---
+++ controllers/api/shoppingcart.js
@@ -0,0 +1,56 @@
+// *********************************************************************************
+// shoppingcart-api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+
+const router = require("express").Router();
+const { User, Book, ShoppingCart } = require("../../models");
+
+//The `\api\shoppingCart` routes
+
+// get all shoppingCart items from user session "Admin"
+router.get("/api/shoppingcart", (req, res) => {
+  // find all  books in shoppingcart
+  // be sure to include its associated User session and Book data
+  ShoppingCart.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ["id", "user_name"],
+      },
+      {
+        model: Book,
+        attributes: ["id", "book_name", "price"],
+      },
+    ],
+  })
+    .then((dbShoppingCartData) => res.json(dbShoppingCartData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Get route for retrieving a single shoppingcart for a Userid
+router.get("/api/shoppingcart/:user_id", withAuth, async (req, res) => {
+  // GET one shopping cart for a Userid
+  // Use the custom middleware before allowing the user to access the shopping cart
+  try {
+    const shoppingCartData = await ShoppingCart.find(req.params.user_id, {
+      include: [
+        {
+          model: User,
+          attributes: ["id"],
+        },
+        {
+          model: Book,
+          attributes: ["id", "book_name", "price"],
+        },
+      ],
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
