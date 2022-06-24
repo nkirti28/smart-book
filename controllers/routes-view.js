a08444716ef2e3460ce5446b30ce67e94264ce38
tj-tieso <tj.tieso@gmail.com>
2022-06-24 08:50:55 -0500

    front end displaying. User/Category Routes still working

--- controllers/routes-view.js
+++ controllers/routes-view.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
+
 router.get("/", (req, res) => {
-  res.render("homepage", {
-  });
+  res.render("homepage", {});
 });
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
-router.get("/logout", (req,res)=>{
+router.get("/logout", (req, res) => {
   res.render("logout");
-})
+});
 
 router.get("/signup", (req, res) => {
   res.render("signup");
