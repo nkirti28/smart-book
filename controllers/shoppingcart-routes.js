0b633de0931ce776940eff76f859010442f59a87
nkirti28 <nalawade.kirti@gmail.com>
2022-06-27 18:59:02 -0500

    cart-view refactor

--- controllers/shoppingcart-routes.js
+++ controllers/shoppingcart-routes.js
@@ -3,8 +3,8 @@ const sequelize = require("../config/connection");
 
 const { User, Book, Category, ShoppingCart } = require("../models");
 
-router.get("/", (req, res) => {
-  res.render("shoppingcart", {loggedIn: true });
+router.get("/shoppingcart", (req, res) => {
+  res.render("shoppingcart", { loggedIn: true });
 });
 
 module.exports = router;
