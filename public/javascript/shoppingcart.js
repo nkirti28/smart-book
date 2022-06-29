7bf777070ba15db54e5a786f40ef2face4eb2f54
nkirti28 <nalawade.kirti@gmail.com>
2022-06-29 17:31:43 -0500

    shoppingcart js added

--- public/javascript/shoppingcart.js
+++ public/javascript/shoppingcart.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   // ******************** Event listeners *********************
   $(document).on("click", (event) => {
     // Continue browsing button clicked
+    console.log("continue browsing button clicked!");
     if ($(event.target).attr("id") === "continueBrowsing") {
       window.location.href = "/category";
     }
